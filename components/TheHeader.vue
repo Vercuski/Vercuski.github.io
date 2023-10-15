452f43884e1d98a7decc3718a93197c2579f5388
Scott Vercuski <lumazi@gmail.com>
2023-10-14 21:21:59 -0400

    Initial Layout with just Vue

--- components/TheHeader.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div class="header">Header</div>
-</template>
--- layouts/default.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <TheHeader></TheHeader>
-  <div class="content">
-    <slot />
-  </div>
-  <TheFooter></TheFooter>
-</template>
